34b1fdfd90c777bb00bbb085be186c7939e2a463
eskici142-rgb <eskici142@gmail.com>
2026-08-01 23:46:38 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+ALICI_EMAIL = "Eskici142@gmail.com"
+
+GONDEREN_EMAIL = os.environ.get("GMAIL_USER")
+GMAIL_PASSWORD = os.environ.get("GMAIL_PASSWORD")
+
+HEADLESS = True
+TIMEOUT_MS = 60_000
